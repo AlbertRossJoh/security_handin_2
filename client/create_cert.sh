57d507c7f6f5d3239a5f4fa43b3a8a44aed2233e
AlbertRossJoh <albertrossj@gmail.com>
2024-10-21 01:17:59 +0200

    cleaned up scripts

--- client/create_cert.sh
+++ client/create_cert.sh
@@ -8,7 +8,6 @@ acc=""
 
 while IFS= read -r line
 do
-  # Append the line to the combined string, separating with commas
   if [ -z "$acc" ]; then
     acc="DNS:$line"
   else
@@ -21,20 +20,18 @@ echo $acc
 echo "subjectAltName=$acc" >> "/var/certs/conf/$HOSTNAME-ext.cnf"
 
 
-# 2. Generate web server's private key and certificate signing request (CSR)
+# Create private key and CSR
 openssl req -newkey rsa:4096 \
     -nodes -keyout /var/keys/clients/$HOSTNAME-key.pem \
     -out /var/certs/$HOSTNAME-req.pem \
     -subj "/C=DK/L=Copenhagen/O=ITU/OU=Education/CN=$HOSTNAME/emailAddress=alrj@itu.dk" > /dev/null 2>&1
 
-# 3. Use CA's private key to sign web server's CSR and get back the signed certificate
+# Sign the cert
 openssl x509 \
     -req -in /var/certs/$HOSTNAME-req.pem \
     -days 60 -CA /var/certs/ca/ca-cert.pem \
     -CAkey /var/certs/ca/ca-key.pem \
     -extfile /var/certs/conf/$HOSTNAME-ext.cnf \
-    -CAcreateserial -out /var/certs/$HOSTNAME-cert.pem #> /dev/null 2>&1
+    -CAcreateserial -out /var/certs/$HOSTNAME-cert.pem > /dev/null 2>&1
 
 go run ./client/
-#echo "Server's signed certificate"
-#openssl x509 -in server-cert.pem -noout -text
--- server/create_cert.sh
+++ server/create_cert.sh
@@ -7,7 +7,6 @@ acc=""
 
 while IFS= read -r line
 do
-  # Append the line to the combined string, separating with commas
   if [ -z "$acc" ]; then
     acc="DNS:$line"
   else
@@ -19,14 +18,14 @@ echo $acc
 
 echo "subjectAltName=$acc" >> "/var/certs/conf/$HOSTNAME-ext.cnf"
 
-# 2. Generate web server's private key and certificate signing request (CSR)
+# Create private key and CSR
 openssl req -newkey rsa:4096 \
     -nodes -keyout /var/keys/clients/$HOSTNAME-key.pem \
     -out /var/certs/$HOSTNAME-req.pem \
     -addext "subjectAltName = $acc" \
     -subj "/C=DK/L=Copenhagen/O=ITU/OU=Education/CN=$HOSTNAME/emailAddress=alrj@itu.dk" > /dev/null 2>&1
 
-# 3. Use CA's private key to sign web server's CSR and get back the signed certificate
+# Sign the cert
 openssl x509 \
     -req -in /var/certs/$HOSTNAME-req.pem \
     -days 60 -CA /var/certs/ca/ca-cert.pem \
@@ -35,5 +34,3 @@ openssl x509 \
     -CAcreateserial -out /var/certs/$HOSTNAME-cert.pem > /dev/null 2>&1
 
 go run ./server/
-#echo "Server's signed certificate"
-#openssl x509 -in server-cert.pem -noout -text
